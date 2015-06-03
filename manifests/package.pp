99cfcf10eb01ea525d95b296a03637342ae5ea63
Kyrill Poole <kyrill@pancentric.com>
2015-06-03 12:55:24 +0100

    changed the installer to use the ES module's style of managing packets

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,7 +1,15 @@
-class packetbeat::package ($version = '1.0.0~Beta1'){
+class packetbeat::package ($deburl = false, $version = '1.0.0~Beta1'){
+  include ::apt
+
+  apt::localpackage { 'packetbeat':
+    url => $deburl ? {
+      false   => "https://download.elasticsearch.org/beats/packetbeat/packetbeat_${version}_amd64.deb",
+      default => $deburl
+    }
+  }
+
   package { 'packetbeat':
-    ensure   => installed,
-    provider => 'dpkg',
-    source   => "https://download.elasticsearch.org/beats/packetbeat/packetbeat_${version}_amd64.deb"
+    ensure  => $version,
+    require => Apt::Localpackage['packetbeat']
   }
 }
