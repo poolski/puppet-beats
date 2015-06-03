845e4d4977e88867f5f3ac5aa8fc660432315db3
Kyrill Poole <kyrill@pancentric.com>
2015-06-03 12:50:31 +0100

    fixed

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,6 +1,6 @@
 class packetbeat::package ($version = '1.0.0~Beta1'){
   package { 'packetbeat':
-    ensure   => $version,
+    ensure   => installed,
     provider => 'dpkg',
     source   => "https://download.elasticsearch.org/beats/packetbeat/packetbeat_${version}_amd64.deb"
   }
