4eac73fd0371585596b39fd363e79d44792e2d82
Kyrill Poole <kyrill@pancentric.com>
2015-06-03 17:24:44 +0100

    moved all the main variables (sniffer, interface, ES, Redis, File) into init to stop the sprawl of variables across params

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,12 +21,43 @@
 # GPLv2
 #
 class packetbeat (
-  $elasticsearch_host = $packetbeat::params::elasticsearch_host,
-  $elasticsearch_port = $packetbeat::params::elasticsearch_port,
-  $interfaces = $packetbeat::params::interfaces,
-  $agentname = $packetbeat::params::agentname,
-) inherits packetbeat::params {
-
+  $agentname = $::fqdn ,
+  $ensure = 'running',
+  $enable = 'true',
+  $uid = '501',
+  $gid = '501',
+  $disable_procs = 'true',
+  $refresh_topology_freq = '10',
+  $topology_expire = '15',
+  $interfaces = 'any',
+  $int_snaplen = '1514',
+  $int_sniffer_type = 'af_packet',
+  $int_buffer_size = '100',
+  $int_ignore_outgoing = 'false',
+  $agentname = $::fqdn,
+  $es_enabled = true,
+  $es_host = 'localhost',
+  $es_port = '9200',
+  $es_username = undef,
+  $es_password = undef,
+  $es_protocol = undef,
+  $es_save_topology = false,
+  $es_index = undef,
+  $es_http_path = undef,
+  $redis_enabled = false,
+  $redis_host = 'localhost',
+  $redis_port = '6379',
+  $redis_reconnect_interval = '1',
+  $redis_save_topology = false,
+  $redis_db_topology = '1',
+  $redis_password = '',
+  $redis_timeout = '5',
+  $redis_index = 'packetbeat',
+  $redis_db = '0',
+  $file_enabled = false,
+  $file_path = '/var/log/'
+) {
   include packetbeat::package, packetbeat::config, packetbeat::service
   Class['packetbeat::package'] -> Class['packetbeat::config'] ~> Class'packetbeat::service']
+  
 }
