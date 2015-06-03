4c791bb345703e3998e9f68da080eba4cb137014
Kyrill Poole <kyrill@pancentric.com>
2015-06-03 16:11:40 +0100

    fleshed out ES output to include all current variables with sensible defaults

--- manifests/outputs/elasticsearch.pp
+++ manifests/outputs/elasticsearch.pp
@@ -1,6 +1,12 @@
 class packetbeat::outputs::elasticsearch (
   $host = $packetbeat::elasticsearch_host,
   $port = $packetbeat::elasticsearch_port,
+  $username = undef,
+  $password = undef,
+  $protocol = undef,
+  $save_topology = false,
+  $index = undef,
+  $http_path = undef,
 ) {
   concat::fragment {'output.elasticsearch':
     target  => '/etc/packetbeat/packetbeat.yml',
