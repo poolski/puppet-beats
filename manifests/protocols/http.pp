080072e057989d1dad779f05e20e9b9771bb902d
Kyrill Poole <kyrill@pancentric.com>
2015-06-03 13:19:53 +0100

    changing keywords checks!

--- manifests/protocols/http.pp
+++ manifests/protocols/http.pp
@@ -1,6 +1,6 @@
 class packetbeat::protocols::http (
   $http_ports = '80, 8080, 8000',
-  $hide_keywords = [],
+  $hide_keywords = undef,
   $send_headers = ["Host", "Cookie", "X-Forwarded-For"],
   $split_cookie = true,
   $real_ip_header = "X-Forwarded-For"
@@ -8,6 +8,6 @@ class packetbeat::protocols::http (
   concat::fragment {"protocols-http":
     target  => '/etc/packetbeat/packetbeat.conf',
     content => template('packetbeat/protocols/protocols.http.erb'),
-    order   => 20,
+    order   => 11,
   }
 }
