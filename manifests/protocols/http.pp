001cb21a0a4e2b280e3e07f3c1acf03f37c390ca
Kyrill Poole <kyrill@pancentric.com>
2015-06-04 15:59:16 +0100

    removed X-Forwarded-For from the send_headers hash

--- manifests/protocols/http.pp
+++ manifests/protocols/http.pp
@@ -1,7 +1,7 @@
 class packetbeat::protocols::http (
   $http_ports = '80, 8080, 8000',
   $hide_keywords = undef,
-  $send_headers = ["Host", "Cookie", "X-Forwarded-For"],
+  $send_headers = ["Host", "Cookie"],
   $split_cookie = true,
   $real_ip_header = "X-Forwarded-For"
 ) {
