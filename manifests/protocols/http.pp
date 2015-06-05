a7b70c3ecdecddae3da5bce21b898b56557c28d9
Kyrill Poole <kyrill@pancentric.com>
2015-06-05 11:04:18 +0100

    modified ports list to be an array of ports for easier iteration and to make it easier to use hiera or something for the values

--- manifests/protocols/http.pp
+++ manifests/protocols/http.pp
@@ -1,9 +1,9 @@
 class packetbeat::protocols::http (
-  $http_ports = '80, 8080, 8000',
+  $http_ports = ['80', '8080', '8000'],
   $hide_keywords = undef,
-  $send_headers = ["Host", "Cookie"],
+  $send_headers = ['Host', 'Cookie'],
   $split_cookie = true,
-  $real_ip_header = "X-Forwarded-For"
+  $real_ip_header = 'X-Forwarded-For'
 ) {
   concat::fragment {"protocols-http":
     target  => '/etc/packetbeat/packetbeat.yml',
