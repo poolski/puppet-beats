e9cba05c7a667873bd503f1aca00e934bee99a49
Kyrill Poole <kyrill@pancentric.com>
2015-06-05 11:08:59 +0100

    hide_keywords is now an array

--- manifests/protocols/http.pp
+++ manifests/protocols/http.pp
@@ -1,6 +1,6 @@
 class packetbeat::protocols::http (
   $http_ports = ['80', '8080', '8000'],
-  $hide_keywords = undef,
+  $hide_keywords = [],
   $send_headers = ['Host', 'Cookie'],
   $split_cookie = true,
   $real_ip_header = 'X-Forwarded-For'
