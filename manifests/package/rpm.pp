5eb2290222fb74de090f62af987e5249efd25956
Kris Buytaert <sdog@inuits.eu>
2015-08-04 17:31:14 +0200

    Removing wget dependency for rpm packges as package takes url as source, also updating params so it finds an actual existing version

--- manifests/package/rpm.pp
+++ manifests/package/rpm.pp
@@ -1,20 +1,13 @@
 class packetbeat::package::rpm inherits packetbeat::package {
 
-  wget::fetch { 'packetbeat':
-    source      => "https://download.elasticsearch.org/beats/packetbeat/packetbeat_${version}_${::architecture}.rpm",
-    destination => "/tmp/packetbeat-${version}_${::architecture}.rpm",
-    timeout     => 0,
-    verbose     => false,
-  }
 
-  package { 'libpcap0.8':
+  package { 'libpcap':
     ensure => installed,
     before => Package['packetbeat']
   }
   package { 'packetbeat':
     ensure   => installed,
     provider => rpm,
-    source   => "/tmp/packetbeat-${version}_${::architecture}.rpm",
-    require  => Wget::Fetch['packetbeat']
+    source   => "https://download.elasticsearch.org/beats/packetbeat/packetbeat-${version}-${::architecture}.rpm",
   }
 }
