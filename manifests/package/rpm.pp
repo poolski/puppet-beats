9cfb7b6a040098f391b301e2f64ecea699b592b2
Steve Womack <Steve.Womack@wwt.com>
2015-08-28 15:55:41 -0500

    Fixing the rpm manifest because there's no repo for this yet

--- manifests/package/rpm.pp
+++ manifests/package/rpm.pp
@@ -1,11 +1,26 @@
 class packetbeat::package::rpm inherits packetbeat::package {
 
-
   package { 'libpcap':
     ensure => installed,
     before => Package['packetbeat']
   }
-  package { 'packetbeat':
-    ensure   => installed,
+
+  remote_file { '/tmp/packetbeat-1.0.0-beta2-x86_64.rpm':
+    ensure      => present,
+    source      => 'https://download.elasticsearch.org/beats/packetbeat/packetbeat-1.0.0-beta2-x86_64.rpm',
+    verify_peer => false,
+    require     => Package['libcap'],
+    before      => Exec['rpm install'],
   }
+
+#  curl -L -O https://download.elasticsearch.org/beats/packetbeat/packetbeat-1.0.0-beta2-x86_64.rpm
+#  sudo rpm -vi packetbeat-1.0.0-beta2-x86_64.rpm
+
+  exec { 'rpm install':
+    command => 'rpm -vi packetbeat-1.0.0-beta2-x86_64.rpm',
+    cwd     => "/tmp",
+    creates => "/usr/bin/packetbeat",
+    path    => ["/usr/bin", "/usr/sbin"]
+  }
+
 }
