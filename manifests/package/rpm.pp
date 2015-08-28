80c4f81a91c8f0c1d156dd488a88e4cb68552d77
Steve Womack <stwomack@gmail.com>
2015-08-28 16:12:54 -0500

    Update rpm.pp

--- manifests/package/rpm.pp
+++ manifests/package/rpm.pp
@@ -2,19 +2,16 @@ class packetbeat::package::rpm inherits packetbeat::package {
 
   package { 'libpcap':
     ensure => installed,
-    before => Package['packetbeat']
+    before => Exec['rpm install'],
   }
 
   remote_file { '/tmp/packetbeat-1.0.0-beta2-x86_64.rpm':
     ensure      => present,
     source      => 'https://download.elasticsearch.org/beats/packetbeat/packetbeat-1.0.0-beta2-x86_64.rpm',
     verify_peer => false,
-    require     => Package['libcap'],
+    require     => Package['libpcap'],
     before      => Exec['rpm install'],
   }
-
-#  curl -L -O https://download.elasticsearch.org/beats/packetbeat/packetbeat-1.0.0-beta2-x86_64.rpm
-#  sudo rpm -vi packetbeat-1.0.0-beta2-x86_64.rpm
 
   exec { 'rpm install':
     command => 'rpm -vi packetbeat-1.0.0-beta2-x86_64.rpm',
