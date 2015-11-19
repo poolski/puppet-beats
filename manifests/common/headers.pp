541e979844e3d5f12254d13dd9e1a05789bfd4f4
Kyrill Poole <kyrill@pancentric.com>
2015-11-19 14:14:21 +0000

    added common stuffs

---
+++ manifests/common/headers.pp
@@ -0,0 +1,28 @@
+define beats::common::headers (
+  $agentname             = $beats::agentname,
+  $tags                  = $beats::tags,
+  $refresh_topology_freq = $beats::refresh_topology_freq,
+  $ignore_outgoing       = $beats::ignore_outgoing,
+) {
+  concat { "/etc/${title}/${title}.yml":
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
+    order  => 'numeric',
+  }
+  concat::fragment {'${title}-commoon-shipper-config':
+    target  => '/etc/${title}/${title}.yml',
+    content => template('beats/shipper.erb'),
+    order   => 01,
+  }
+  concat::fragment {'${title}-common-runopts':
+    target  => '/etc/${title}/${title}.yml',
+    content => template('beats/runopts.erb'),
+    order   => 02,
+  }
+  concat::fragment {"${title}-common-output-header":
+    target  => "/etc/${title}/${title}.yml",
+    content => template('beats/outputs/output.header.erb'),
+    order   => 20
+  }
+}
