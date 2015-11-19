dc45ecccf219c6927e9ea97af15933c0cd854346
Kyrill Poole <kyrill@pancentric.com>
2015-11-19 09:41:26 +0000

    made apt work

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,14 +1,16 @@
 # Setup the official repo
-apt::source { 'elastic-beats':
-	comment => 'Official Elastic beats repositories',
-	location => 'https://packages.elastic.co/beats/apt',
-	release => 'stable',
-	repos => 'main',
-	key => {
-		id => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-		server => 'pgp.mit.edu'
-	},
-	include => {
-		'deb' => true
-	}
+class beats::repo::apt() {
+  apt::source { 'elastic-beats':
+    comment => 'Official Elastic beats repositories',
+    location => 'https://packages.elastic.co/beats/apt',
+    release => 'stable',
+    repos => 'main',
+    key => {
+      id => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+      server => 'pgp.mit.edu'
+    },
+    include => {
+      'deb' => true
+    }
+  }
 }
