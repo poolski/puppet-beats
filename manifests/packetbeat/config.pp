f82aee1ca50aef05e069cf5e0c78a245052b836d
Kyrill Poole <kyrill@pancentric.com>
2015-11-19 13:35:34 +0000

    altered targets for packetbeat config generator

--- manifests/packetbeat/config.pp
+++ manifests/packetbeat/config.pp
@@ -5,6 +5,22 @@ class beats::packetbeat::config(
   $int_sniffer_type = undef,
   $int_buffer_size  = undef,
 ){
+  concat { '/etc/packetbeat/packetbeat.yml':
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
+    order  => 'numeric',
+  }
+  concat::fragment {'packetbeat-commoon-shipper-config':
+    target  => '/etc/packetbeat/packetbeat.yml',
+    content => template('beats/shipper.erb'),
+    order   => 01,
+  }
+  concat::fragment {'packetbeat-common-runopts':
+    target  => '/etc/packetbeat/packetbeat.yml',
+    content => template('beats/runopts.erb'),
+    order   => 02,
+  }
   concat::fragment {'packetbeat.header':
     target  => '/etc/packetbeat/packetbeat.yml',
     content => template('beats/packetbeat/packetbeat.yml.erb'),
