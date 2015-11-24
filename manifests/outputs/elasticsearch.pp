11cd9dec4e40e989f9ed5d49718230a3538c9592
Kyrill Poole <kyrill@pancentric.com>
2015-11-24 11:07:45 +0000

    removed extra nonsense from ES output manifest

--- manifests/outputs/elasticsearch.pp
+++ manifests/outputs/elasticsearch.pp
@@ -3,26 +3,9 @@ define beats::outputs::elasticsearch (
   $save_topology = true,
   $index         = 'packetbeat',
 ) {
-
-  if $beats::es_enabled {
-    concat::fragment {"${title}-output-elasticsearch":
-      target  => "/etc/${title}/${title}.yml",
-      content => template('beats/outputs/elasticsearch.erb'),
-      order   => 21
-    }
-  }
-  if $beats::redis_enabled {
-    concat::fragment {"${title}-output-redis":
-      target  => "/etc/${title}/${title}.yml",
-      content => template('beats/outputs/redis.erb'),
-      order   => 22
-    }
-  }
-  if $beats::file_enabled {
-    concat::fragment {"${title}-output-file":
-      target  => "/etc/${title}/${title}.yml",
-      content => template('beats/outputs/file.erb'),
-      order   => 23
-    }
+  concat::fragment {"${title}-output-elasticsearch":
+    target  => "/etc/${title}/${title}.yml",
+    content => template('beats/outputs/elasticsearch.erb'),
+    order   => 21
   }
 }
