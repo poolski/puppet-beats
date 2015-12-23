0425a60caafafb9fa292db58f55295daca944890
Kyrill Poole <kyrill@pancentric.com>
2015-12-23 15:29:21 +0000

    added filebeat

---
+++ manifests/filebeat.pp
@@ -0,0 +1,29 @@
+class beats::filebeat (
+  $ensure        = present,
+  $idle_timeout  = '5s',
+  $prospectors   = {},
+  $registry_file = '/var/lib/filebeat/registry',
+  $spool_size    = '1024',
+){
+
+  beats::common::headers {'filebeat':}
+  concat::fragment {'filebeat.header':
+    target  => '/etc/filebeat/filebeat.yml',
+    content => template('beats/filebeat/filebeat.yml.erb'),
+    order   => 05,
+  }
+
+  include ::apt::update
+
+  package {'filebeat':
+    ensure => $ensure,
+    require => Class['apt::update']
+  }
+  service { 'filebeat':
+    ensure => running,
+    enable => true,
+  }
+  create_resources('::beats::filebeat::prospector', $prospectors)
+
+  Package['filebeat'] -> Concat::Fragment['filebeat'] -> Create_Resources['::beats::filebeat::prospectors'] ~> Service['filebeat']
+}
---
+++ manifests/filebeat/prospector.pp
@@ -0,0 +1,22 @@
+define beats::filebeat::prospector(
+  $paths                 = [ '/var/log/syslog' ],
+  $fields                = { type => "syslog"},
+  $encoding              = 'plain',
+  $input_type            = 'log',
+  $ignore_older          = '24h',
+  $document_type         = 'log',
+  $scan_frequency        = '10s',
+  $harvester_buffer_size = '16384',
+  $tail_files            = true
+  $backoff               = '1s',
+  $max_backoff           = '10s',
+  $backoff_factor        = '2',
+  $partial_line_waiting  = '5s',
+  $force_close_files     = false,
+){
+  concat::fragment {"prospector-$title":
+    target  => '/etc/filebeat/filebeat.yml',
+    content => template('beats/filebeat/prospector.yml.erb'),
+    order   => 15,
+  }
+}
