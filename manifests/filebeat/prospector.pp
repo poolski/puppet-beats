8eff1ed8e297e0710bd6f8dfa37f0243a77ef959
Patrik Majer <patrik.majer@atlas.cz>
2016-03-16 23:03:10 +0100

    fix permission for config files

--- manifests/filebeat/prospector.pp
+++ manifests/filebeat/prospector.pp
@@ -18,5 +18,6 @@ define beats::filebeat::prospector(
     target  => '/etc/filebeat/filebeat.yml',
     content => template('beats/filebeat/prospector.yml.erb'),
     order   => 17,
+    mode    => '0644',
   }
 }
