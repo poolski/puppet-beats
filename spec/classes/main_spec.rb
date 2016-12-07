25f66adda8ba4e502033e3d5904a8de3c70022f0
Patrik Majer <patrik.majer@atlas.cz>
2016-12-07 14:07:27 +0100

    fix test specs

--- spec/classes/main_spec.rb
+++ spec/classes/main_spec.rb
@@ -44,4 +44,58 @@ describe 'beats' do
       it { expect { is_expected.to contain_package('beats') }.to raise_error(Puppet::Error, /Solaris not supported yet/) }
     end
   end
+
+  context 'RedHat/CentOS 6 operaton system' do
+    describe 'beats class without any parameters on RedHat/CentOS 6' do
+       let (:facts) {{
+         :osfamily                  => 'RedHat',
+         :operatingsystem           => 'RedHat',
+         :operatingsystemmajrelease => '6',
+         :architecture              => 'x86_64',
+       }}
+       it {
+         is_expected.to contain_class('beats::repo::yum')
+         is_expected.to contain_class('beats::repo::yum').that_comes_before('beats::package')
+
+         is_expected.to contain_package('GeoIP').with_ensure('latest')
+         is_expected.to contain_package('libpcap').with_ensure('installed')
+       }
+    end
+  end
+
+  context 'Debian operation system' do
+    describe 'beats class without any parameters on Debian' do
+       let (:facts) {{
+         :osfamily                  => 'Debian',
+         :lsbdistid                 => 'Debian'
+       }}
+       it {
+         is_expected.to contain_class('beats::repo::apt')
+         is_expected.to contain_class('beats::repo::apt').that_comes_before('beats::package')
+
+         is_expected.to contain_package('geoip-database-contrib').with_ensure('latest')
+         is_expected.to contain_package('apt-transport-https').with_ensure('latest')
+         is_expected.to contain_package('libpcap0.8').with_ensure('installed')
+       }
+    end
+  end
+
+  context 'Ubuntu operation system' do
+    describe 'beats class without any parameters on Ubuntu' do
+       let (:facts) {{
+         :osfamily                  => 'Debian',
+         :lsbdistid                 => 'Ubuntu'
+       }}
+       it {
+         is_expected.to contain_class('beats::repo::apt')
+         is_expected.to contain_class('beats::repo::apt').that_comes_before('beats::package')
+
+         is_expected.to contain_package('geoip-database-contrib').with_ensure('latest')
+         is_expected.to contain_package('apt-transport-https').with_ensure('latest')
+         is_expected.to contain_package('libpcap0.8').with_ensure('installed')
+       }
+    end
+  end
+
 end
+
